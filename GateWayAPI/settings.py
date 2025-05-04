db8501c32db9c37e8c72825f5df9a7743ffb2912
Matidza <Matidza46@gmail.com>
2025-05-04 03:41:18 +0200

    Mailtrap

--- GateWayAPI/settings.py
+++ GateWayAPI/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vyuzze@gmail.com'  # Fetch from environment variables
+EMAIL_HOST_USER = 'vyuzes@gmail.com'  # Fetch from environment variables
 EMAIL_HOST_PASSWORD = 'Dlta123!@#' # Fetch from environment variables
